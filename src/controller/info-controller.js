f82f70e475dc21f84a775d8749418c20eed2c202
KalpeshJangir23 <21lunagok@gmail.com>
2025-10-26 22:46:35 +0530

    intialize

---
+++ src/controller/info-controller.js
@@ -0,0 +1,16 @@
+const statusCode = require('http-status-codes');
+
+
+const info = (req, res) => {
+         return res.status(statusCode.OK).json({
+                  success: true,
+                  message: 'API IS LIVE',
+                  error: {},
+                  data:{}
+         })
+}
+
+
+module.exports = {
+         info
+}
